14a20679e8db20525accf25af3d7a62999032764
Wuzzy <almikes@aol.com>
2016-11-09 02:29:06 +0100

    Fix outdated intllib boilerplate

--- init.lua
+++ init.lua
@@ -1,8 +1,7 @@
 -- Boilerplate to support localized strings if intllib mod is installed.
 local S
 if minetest.get_modpath("intllib") then
-	dofile(minetest.get_modpath("intllib").."/intllib.lua")
-	S = intllib.Getter(minetest.get_current_modname())
+	S = intllib.Getter()
 else
 	S = function(s) return s end
 end
